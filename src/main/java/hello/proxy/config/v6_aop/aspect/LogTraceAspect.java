4dfdc5ac31cf042e03bb589498d8e19a6e77c1ce
jongyounlee <jayylee.dev@gmail.com>
2023-10-30 10:04:45 +0900

    7. bean post processor

---
+++ src/main/java/hello/proxy/config/v6_aop/aspect/LogTraceAspect.java
@@ -0,0 +1,19 @@
+package hello.proxy.config.v6_aop.aspect;
+
+import hello.proxy.trace.logtrace.LogTrace;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Slf4j
+//@Aspect
+public class LogTraceAspect {
+
+//    private final LogTrace logTrace;
+//
+//    public LogTraceAspect(LogTrace logTrace) {
+//        this.logTrace = logTrace;
+//    }
+//
+//    @Around("execution(* hello.proxy.app..*(..))")
+//    public Object execute(ProceedingJoinPoint)
+}
